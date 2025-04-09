2f93f4a0bf21488f29a1212361352d2a17492f49
Altobert <alberto.sanmartin@usach.cl>
2025-04-09 00:23:53 +0000

    se agregan comentarios para agregar el sitio completo

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import Header from "@/app/components/header";
 import ChatSection from "./components/chat-section";
 
+/**
+ * 
+ * @returns
+ * <ChatSection /> 
+ */
 export default function Home() {
   return (
     <main className="h-screen w-screen flex justify-center items-center background-gradient">
       <div className="space-y-2 lg:space-y-10 w-[90%] lg:w-[60rem]">
         <Header />
-        <div className="h-[65vh] flex">
-          <ChatSection />
+        <div className="h-[65vh] flex">          
+          
         </div>
       </div>
     </main>
