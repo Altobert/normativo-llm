653cce33fff019ebe572a3471e71c1704e459d32
Altobert <alberto.sanmartin@usach.cl>
2025-05-01 22:52:46 +0000

    se agrega busqueda

--- app/components/body-search-norma.tsx
+++ app/components/body-search-norma.tsx
@@ -6,13 +6,41 @@ import React, { useState } from 'react';
 const BodySearchNorma: React.FC = () => {
     const [query, setQuery] = useState('');
 
-    const handleSearch = (e: React.FormEvent) => {
+    // Función para manejar el envío del formulario con el texto completo para buscar
+
+    const handleSearch = async () => {
+        if (!query.trim()) {
+          alert('Por favor, ingresa un término de búsqueda.');
+          return;
+        }
+    
+        try {
+          const response = await fetch(`http://127.0.0.1:8080/api/search?query=${encodeURIComponent(query)}`, {
+            method: 'GET',
+          });
+    
+          if (response.ok) {
+            const data = await response.json();
+            console.log('Resultados de la búsqueda:', data);
+            alert('Búsqueda realizada con éxito. Revisa la consola para ver los resultados.');
+          } else {
+            alert('Error al realizar la búsqueda.');
+          }
+        } catch (error) {
+          console.error('Error al realizar la búsqueda:', error);
+          alert('Ocurrió un error al realizar la búsqueda.');
+        }
+      };
+    
+    /*const handleSearch = (e: React.FormEvent) => {
         e.preventDefault();
         console.log('Buscando:', query);
-        // Aca agregar la logica para realizar la búsqueda
-    };
+        // Aquí puedes agregar la lógica para realizar la búsqueda
+        // Por ejemplo, puedes hacer una llamada a una API o filtrar una lista de proyectos normativos
+        // y mostrar los resultados en la interfaz de usuario.
+        // Puedes usar la función fetch para hacer una solicitud a tu API
 
-    return (
+
         <div style={{ textAlign: 'center', marginTop: '50px', width: '100%' }}>
             <form onSubmit={handleSearch}>
                 <input
@@ -50,6 +78,26 @@ const BodySearchNorma: React.FC = () => {
             </form>
             
         </div>
+                                
+    };*/
+
+    return (
+        <div className="container mx-auto p-10">
+      <h1 className="text-2xl font-bold mb-4">Buscador</h1>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Escribe tu búsqueda aquí..."
+        className="border border-gray-300 rounded py-2 px-4 w-full mb-4"
+      />
+      <button
+        onClick={handleSearch}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Buscar
+      </button>
+    </div>
     );
 };
 
